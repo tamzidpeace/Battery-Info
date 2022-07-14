0741d67a47b3e680ab8fef0a75f4e5da6bc9b79a
tamzidpeace <tamjedpeace@gmail.com>
2022-07-14 23:59:12 +0600

    ---

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:battery_plus/battery_plus.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -59,7 +58,9 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final Battery _battery = Battery();
-  int batterLevel = 0;
+  int _batterLevel = 0;
+  final TextEditingController _maxLevelController = TextEditingController(text: '99');
+  final TextEditingController _minLevelController = TextEditingController(text: '30');
 
   BatteryState? _batteryState;
   StreamSubscription<BatteryState>? _batteryStateSubscription;
@@ -73,29 +74,9 @@ class _MyHomePageState extends State<MyHomePage> {
     /// batter related
     _battery.batteryState.then(_updateBatteryState);
     _batteryStateSubscription = _battery.onBatteryStateChanged.listen(_updateBatteryState);
-
-    //runs the function every 60 seconds
-    Timer.periodic(const Duration(seconds: 60), (Timer t) {
-      _battery.batteryLevel.then((int level) {
-        setState(() {
-          batterLevel = level;
-        });
-        if (kDebugMode) {
-          print(batterLevel.toDouble());
-        }
-        if (level >= 80) {
-          _showNotificationWithNoTitle(level: level);
-        }
-      });
-    });
   }
 
   void _requestPermissions() {
-    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
-          alert: true,
-          badge: true,
-          sound: true,
-        );
     flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<MacOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
           alert: true,
           badge: true,
@@ -103,64 +84,27 @@ class _MyHomePageState extends State<MyHomePage> {
         );
   }
 
-  void _updateBatteryState(BatteryState state) {
+  void _updateBatteryState(BatteryState state) async {
+    int level = await _battery.batteryLevel;
+    debugPrint('_updateBatteryState: $level');
+    setState(() {
+      _batterLevel = level;
+    });
+
+    _showNotificationWithNoTitle(
+      level: level,
+      max: int.parse(_maxLevelController.text),
+      min: int.parse(_minLevelController.text),
+    );
+
     if (_batteryState == state) return;
+
     setState(() {
       _batteryState = state;
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      // remove debug banner
-
-      appBar: AppBar(
-        title: const Text('Battery Info'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Batter Level: $batterLevel'),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () async {
-                final batteryLevel = await _battery.batteryLevel;
-                // ignore: unawaited_futures
-                showDialog<void>(
-                  context: context,
-                  builder: (_) => AlertDialog(
-                    content: Text('Battery: $batteryLevel%'),
-                    actions: <Widget>[
-                      TextButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        child: const Text('OK'),
-                      )
-                    ],
-                  ),
-                );
-              },
-              child: const Text('Get battery level'),
-            ),
-            ElevatedButton(onPressed: () => _showNotificationWithNoTitle(level: 10), child: Text('data'))
-          ],
-        ),
-      ),
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (_batteryStateSubscription != null) {
-      _batteryStateSubscription!.cancel();
-    }
-  }
-
-  Future<void> _showNotificationWithNoTitle({required int level}) async {
+  Future<void> _showNotificationWithNoTitle({required int level, int max = 99, min = 30}) async {
     await flutterLocalNotificationsPlugin.cancel(0);
 
     MacOSNotificationDetails macosPlatformChannelSpecifics = MacOSNotificationDetails(
@@ -174,7 +118,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     var message = 'Battery Level: $level%';
 
-    if (level >= 99) {
+    if (level >= max) {
       message = 'Battery Level: $level% - Please unplug the charger';
 
       flutterLocalNotificationsPlugin.show(
@@ -183,7 +127,7 @@ class _MyHomePageState extends State<MyHomePage> {
         message,
         platformChannelSpecifics,
       );
-    } else if (level <= 30) {
+    } else if (level <= min) {
       message = 'Battery Level: $level% - Please plug the charger';
 
       flutterLocalNotificationsPlugin.show(
@@ -194,4 +138,70 @@ class _MyHomePageState extends State<MyHomePage> {
       );
     }
   }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (_batteryStateSubscription != null) {
+      _batteryStateSubscription!.cancel();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // remove debug banner
+
+      appBar: AppBar(
+        title: const Text('Battery Info'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // battery info
+            Text('Batter Level: $_batterLevel'),
+
+            // max level
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Set Max Warning Level',
+                ),
+                controller: _maxLevelController,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child: const Text('Save Changes'),
+            ),
+
+            // min level
+            const SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.number,
+                controller: _minLevelController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Set Min Warning Level',
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () => {},
+              child: const Text('Save Changes'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  // end
 }
