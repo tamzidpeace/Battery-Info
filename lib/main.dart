0e11fd17803839bdaea4454adb5c7e40b55a72ab
tamzidpeace <tamjedpeace@gmail.com>
2022-07-14 23:02:12 +0600

    showing warning workiing

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,30 @@ import 'dart:async';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:battery_plus/battery_plus.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+Future<void> main() async {
+  // needed if you intend to initialize in the `main` function
+  WidgetsFlutterBinding.ensureInitialized();
+
+  const MacOSInitializationSettings initializationSettingsMacOS = MacOSInitializationSettings(
+    requestAlertPermission: false,
+    requestBadgePermission: false,
+    requestSoundPermission: false,
+  );
+
+  const InitializationSettings initializationSettings = InitializationSettings(
+    macOS: initializationSettingsMacOS,
+  );
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings, onSelectNotification: (String? payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: $payload');
+    }
+
+    debugPrint('flutterLocalNotificationsPlugin.initialize: here');
+  });
+
   runApp(const MyApp());
 }
 
@@ -13,14 +35,18 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Battery Info',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Home'),
     );
   }
 }
+
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, this.title}) : super(key: key);
@@ -41,6 +67,10 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
+    // notification related
+    _requestPermissions();
+
+    /// batter related
     _battery.batteryState.then(_updateBatteryState);
     _batteryStateSubscription = _battery.onBatteryStateChanged.listen(_updateBatteryState);
 
@@ -53,8 +83,24 @@ class _MyHomePageState extends State<MyHomePage> {
         if (kDebugMode) {
           print(batterLevel.toDouble());
         }
+        if (level >= 80) {
+          _showNotificationWithNoTitle(level: level);
+        }
       });
     });
+  }
+
+  void _requestPermissions() {
+    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<IOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
+    flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<MacOSFlutterLocalNotificationsPlugin>()?.requestPermissions(
+          alert: true,
+          badge: true,
+          sound: true,
+        );
   }
 
   void _updateBatteryState(BatteryState state) {
@@ -99,6 +145,7 @@ class _MyHomePageState extends State<MyHomePage> {
               },
               child: const Text('Get battery level'),
             ),
+            ElevatedButton(onPressed: () => _showNotificationWithNoTitle(level: 10), child: Text('data'))
           ],
         ),
       ),
@@ -110,6 +157,41 @@ class _MyHomePageState extends State<MyHomePage> {
     super.dispose();
     if (_batteryStateSubscription != null) {
       _batteryStateSubscription!.cancel();
+    }
+  }
+
+  Future<void> _showNotificationWithNoTitle({required int level}) async {
+    await flutterLocalNotificationsPlugin.cancel(0);
+
+    MacOSNotificationDetails macosPlatformChannelSpecifics = MacOSNotificationDetails(
+      subtitle: 'Current Battery Level: $level%',
+      presentAlert: true,
+      presentSound: true,
+      presentBadge: true,
+    );
+
+    NotificationDetails platformChannelSpecifics = NotificationDetails(macOS: macosPlatformChannelSpecifics);
+
+    var message = 'Battery Level: $level%';
+
+    if (level >= 99) {
+      message = 'Battery Level: $level% - Please unplug the charger';
+
+      flutterLocalNotificationsPlugin.show(
+        0,
+        'Warning',
+        message,
+        platformChannelSpecifics,
+      );
+    } else if (level <= 30) {
+      message = 'Battery Level: $level% - Please plug the charger';
+
+      flutterLocalNotificationsPlugin.show(
+        0,
+        'Warning',
+        message,
+        platformChannelSpecifics,
+      );
     }
   }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,9 @@ import FlutterMacOS
 import Foundation
 
 import battery_plus_macos
+import flutter_local_notifications
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BatteryPlusMacosPlugin.register(with: registry.registrar(forPlugin: "BatteryPlusMacosPlugin"))
+  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
 }
